59fc29d545cce459d3ad2f0522918ee4b5595500
noor code <noorcode09@gmail.com>
2025-03-02 17:05:47 +0530

    create gradient card

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar({ className }: { className?: string }) {
     className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-          <MenuItem active={active} setActive={setActive} item="Services">
+          <MenuItem active={active} setActive={setActive} item="Home">
             <div className="flex flex-col space-y-4 text-sm">
                 <HoveredLink href="/web-dev">Web Development</HoveredLink>
                 <HoveredLink href="/web-dev">Web Development</HoveredLink>
@@ -21,6 +21,7 @@ function Navbar({ className }: { className?: string }) {
                 <HoveredLink href="/web-dev">Web Development</HoveredLink>
             </div>
           </MenuItem>
+          
           <MenuItem active={active} setActive={setActive} item="Our Courses">
             <div className="flex flex-col space-y-4 text-sm">
                 <HoveredLink href="/web-dev">Web Development</HoveredLink>
